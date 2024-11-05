99c3cd2d418c5b255ba974af6fb5b78e48c7f0dc
rejipmathew <Rejipmathew@users.noreply.github.com>
2024-11-05 15:27:27 -0500

    Update adsl_streamlit3.py

--- adsl_streamlit3.py
+++ adsl_streamlit3.py
@@ -34,7 +34,7 @@ def main():
     <style>
     .reportview-container .markdown-text-container { font-family: monospace; }
     .sidebar .sidebar-content { background-image: linear-gradient(#2e7bcf,#2e7bcf); color: white; }
-    .Widget>label { color: white; font-family: monospace; }
+    .Widget>label { color: blue; font-family: monospace; }
     [class^="st-b"]  { color: white; font-family: monospace; }
     .st-bb { background-color: transparent; }
     .st-at { background-color: #0c0080; }
